c6de69ffc0379ca50c67bb8112b2413e315553fb
Matthew Vaughn <vaughn@tacc.utexas.edu>
2015-09-11 04:21:43 -0700

    Solidify recipe in common.sh for app/data volume

--- ncbi-blast-2.2.30/common.sh
+++ ncbi-blast-2.2.30/common.sh
@@ -1,26 +1,26 @@
 # Common setup for Docker-based BLAST or other
 # applications with data container
 
-DOCKER_APP_IMAGE='araport/ncbi-blast:2.2.30'
-#DOCKER_DATA_IMAGE='araport/blastdb:tair10'
-DOCKER_DATA_IMAGE='agaveapi/java-api-base'
+DOCKER_APP_IMAGE='araport/agave-ncbi-blast:2.2.30'
+DOCKER_DATA_IMAGE='araport/agave-ncbi-blastdb:tair10'
 
-HOST_SCRATCH='/scratch'
-STAMP=$(date +%s | md5)
+HOST_SCRATCH='/home'
+STAMP=$(date +%s)
 
 ## NO USER-SERVICABLE PARTS INSIDE
 # Create unique but human comprehensible name for each container
 DOCKER_APP_CONTAINER="app-$STAMP"
 DOCKER_DATA_CONTAINER="db-$STAMP"
-HOST_OPTS="--net=none -m=1g --rm=true"
+HOST_OPTS="--net=none -m=1g"
 
 #DOCKER_DATA_RUN="docker run ${HOST_OPTS} --name ${DOCKER_DATA_CONTAINER} $DOCKER_DATA_IMAGE true"
-DOCKER_DATA_RUN="docker run ${HOST_OPTS} --name ${DOCKER_DATA_CONTAINER} $DOCKER_DATA_IMAGE true"
+DOCKER_DATA_RUN="docker create ${HOST_OPTS} --name ${DOCKER_DATA_CONTAINER} $DOCKER_DATA_IMAGE"
 DOCKER_APP_RUN="docker run ${HOST_OPTS} --name ${DOCKER_APP_CONTAINER} -i -t --volumes-from ${DOCKER_DATA_CONTAINER} -v `pwd`:${HOST_SCRATCH}:rw -w ${HOST_SCRATCH} ${DOCKER_APP_IMAGE}"
 ## NO USER-SERVICABLE PARTS INSIDE
 
 # Launch the data container
 ${DOCKER_DATA_RUN}
+${DOCKER_APP_RUN} /bin/bash
 
 exit 0
 
